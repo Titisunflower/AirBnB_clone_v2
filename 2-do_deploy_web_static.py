3453ad5e1903239246fe69071780b0b0ebda7402
Titisunflower <henuwz@gmail.com>
2023-07-10 13:20:21 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Python module to deploy archive on web servers"""
-from fabric.api import env, put, run
+"""Compress web static package
+"""
+from fabric.api import *
+from datetime import datetime
 from os import path
+
 
 env.hosts = ['54.88.208.12', '35.174.208.81']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
@@ -15,30 +18,39 @@ def do_deploy(archive_path):
                 if not (path.exists(archive_path)):
                         return False
 
+                # upload archive
                 put(archive_path, '/tmp/')
 
+                # create target dir
                 timestamp = archive_path[-18:-4]
                 run('sudo mkdir -p /data/web_static/\
 releases/web_static_{}/'.format(timestamp))
 
+                # uncompress archive and delete .tgz
                 run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
 /data/web_static/releases/web_static_{}/'
                     .format(timestamp, timestamp))
 
+                # remove archive
                 run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
+                # move contents into host web_static
                 run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
 /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
+                # remove extraneous web_static dir
                 run('sudo rm -rf /data/web_static/releases/\
 web_static_{}/web_static'
                     .format(timestamp))
 
+                # delete pre-existing sym link
                 run('sudo rm -rf /data/web_static/current')
 
+                # re-establish symbolic link
                 run('sudo ln -s /data/web_static/releases/\
 web_static_{}/ /data/web_static/current'.format(timestamp))
         except:
                 return False
 
+        # return True on success
         return True
