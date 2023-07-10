8883e9621cfd84575961acee3a69d91cfbd6ea68
Titisunflower <henuwz@gmail.com>
2023-07-10 13:03:22 -0700

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """Python module to compress all web static files"""
-from fabric.api import local
-from time import strftime
+from fabric.api import local, task
+from datetime import datetime
 
-
+@task
 def do_pack():
-    """A function that generates .tgz archive from contents of web_static"""
+    """Generates a .tgz archive from the contents of the web_static folder."""
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(timestamp)
 
-    name = strftime('%Y%m%d%H%M%S')
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static".format(name))
-        return ("versions/web_static_{}.tgz".format(name))
-    except Exception:
+    local("mkdir -p versions")
+    result = local("tar -czvf {} web_static".format(archive_path), capture=True)
+
+    if result.succeeded:
+        return archive_path
+    else:
         return None
